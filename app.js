46125dab97b1ff5f6e1cd31fe60bb918680be84d
longlonglu <48895042+longlonglu@users.noreply.github.com>
2021-07-19 11:48:13 -0400

    ok

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ app.use(express.json())
 app.use("/questions", questionsRoute)
 app.use("/auth", authRoute)
 app.use('/userRecord', userRecordRoute)
+app.get('/', (req, res) => { res.json("Hello World")})
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
